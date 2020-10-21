be495d9ae43f8884bf9d486e828987bcc9ae55b1
hardikgoenka71 <72194931+hardikgoenka71@users.noreply.github.com>
2020-10-21 18:46:14 +0545

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,8 @@
+class Log extends BaseClass{
+  constructor(x,y,width,angle)
+  {
+      super(x,y,width,20,angle)
+      this.image=loadImage("sprites/wood2.png");
+      //Matter.body.setAngle(this.body,angle);
+  }
+}
